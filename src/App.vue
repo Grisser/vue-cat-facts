4ce647f7f1a19136c5d0bf67956389946595a95e
Вячеслав Рыжков <rugrisser@gmail.com>
2019-12-20 23:53:45 +0300

    Facts + Interval

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Fact
+            v-bind:fact="fact"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Fact from "@/components/Fact";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                fact: ""
+            }
+        },
+        components: {
+            Fact
+        },
+        methods: {
+            update() {
+                this.axios
+                    .get("http://188.225.47.187/api/cats/random-fact.php")
+                    .then((response) => {
+                        this.fact = response.data;
+                    });
+            }
+        },
+        mounted() {
+            this.update();
+            setInterval(this.update, 10000);
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        padding: 0;
+        margin: 0;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
